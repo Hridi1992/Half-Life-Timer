6bf8ac46e77806c942011e576896e661d73459b6
Darin Cousens <darin.cousens@gmail.com>
2024-07-02 12:39:29 -0500

    Update half-life.v

--- src/half-life.v
+++ src/half-life.v
@@ -5,14 +5,14 @@ input clk, rst, up ;
   input [7:0] load ;
 input [n-1:0] in ;
 output [n-1:0] out ;
-wire [n-1:0] out1 ;
-reg [n-1:0] next ;
+wire [n-1:0] out ;
+  reg [10:0] next ;
 
   DFF #(n) count(next, clk, out) ;
 
 always@(rst, up, down, load, in, out) begin
 casex({rst, up, down, load})
-next: out1 = 4'b0001;
+next: out = 4'b0001;
 //4'bl000: next = {4{1'b0}} ;
 //4'b0100: next = out + 1'bl ;
 //4'b0010: next = out - 1'b1 ;
