76404069f59cd71e7daa090a244549445695843a
Darin Cousens <darin.cousens@gmail.com>
2024-07-02 11:50:58 -0500

    Update half-life.v

--- src/half-life.v
+++ src/half-life.v
@@ -10,7 +10,7 @@ DFF #(n) count(clk, next, out) ;
 
 always@(rst, up, down, load, in, out) begin
 casex({rst, up, down, load})
-next = 4'b0001;
+next: = 4'b0001;
 //4'bl000: next = {4{1'b0}} ;
 //4'b0100: next = out + 1'bl ;
 //4'b0010: next = out - 1'b1 ;
