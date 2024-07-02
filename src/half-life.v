e78342654f4d161f83b7af02cd6cf693e137239c
Darin Cousens <darin.cousens@gmail.com>
2024-07-02 12:31:39 -0500

    Update half-life.v

--- src/half-life.v
+++ src/half-life.v
@@ -1,6 +1,8 @@
 module tt_um_halflife(clk, rst, up, down, load, in, out) ;
 parameter n = 4 ;
-input clk, rst, up, down, load ;
+input clk, rst, up ; 
+  input [2:0] down ;
+  input [5:0] load ;
 input [n-1:0] in ;
 output [n-1:0] out ;
 wire [n-1:0] out1 ;
