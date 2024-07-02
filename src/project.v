56a503760e3a9b1e6a992de1146baac4dd604978
Darin Cousens <darin.cousens@gmail.com>
2024-07-02 10:31:21 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -17,11 +17,25 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+
   assign uio_out = 0;
   assign uio_oe  = 0;
+    assign ui_in[7:6] = 2b'00;
+    assign ui_out[7:4] = 4b'0000;
+    assign uio_in[7:4] = 4b'0000;
+    
 
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
 
+    tt_um_half-life top(
+        .clk(clk),
+        .reset(rst_n),
+        .up(ui_in[4]),
+        .down(ui_in[5]),
+        .load(uio_in[3:0]),
+        .in(ui_in[3:0]),
+        .out(uo_out[3:0]),
+        
+        
 endmodule
