5b9317c44e9fd660471a4ada4f6feab117bf0d68
Hridi Prova Debnath <111787740+Hridi1992@users.noreply.github.com>
2024-07-08 14:07:17 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -21,7 +21,7 @@ module tt_um_example (
   assign uio_out = 0;
   assign uio_oe  = 0;
     //assign ui_in[7:6] = 2'b00;
-    assign uo_out[7:4] = 4'b0000;
+   // assign uo_out[7:4] = 4'b0000;
     //assign uio_in[7:1] = 6'b000000;
 
     
@@ -36,7 +36,7 @@ module tt_um_example (
         .down(ui_in[7:5]),
         .load(uio_in[7:0]),
         .in(ui_in[3:0]),
-        .out(uo_out[3:0])
+        .out(uo_out[7:0])
     )
 
         ;
