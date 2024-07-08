3de3bd0ce7257d3e194e9dbd3da9761936d46869
Hridi Prova Debnath <111787740+Hridi1992@users.noreply.github.com>
2024-07-08 15:32:52 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -30,7 +30,7 @@ module tt_um_example (
     assign zach = ui_in[6:5];
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+    wire _unused = &{ena, 1'b0, rst_n, 1'b0};
 
     tt_um_halflife top(
         .clk(clk),
