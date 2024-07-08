137807432ad750130aa124b4987753fe7768af69
Hridi Prova Debnath <111787740+Hridi1992@users.noreply.github.com>
2024-07-08 11:14:16 -0500

    Update DFF.v

--- src/DFF.v
+++ src/DFF.v
@@ -1,10 +1,11 @@
 module DFF (d, clk, q);
+  //width = 1?
   parameter WIDTH = 1;
   input clk;
   input [WIDTH-1:0] d;
   output [WIDTH-1:0] q;
   reg [WIDTH-1:0] q;
-
+//output and reg same?
   always@ (posedge clk)
     q <= d;
 endmodule
